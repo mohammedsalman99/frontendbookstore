b6e08da14c33e621b33f735857aa73cae3d182c1
Record Don <mhumdanwerdw@gmail.com>
2024-12-02 13:46:47 +0200

    best authors

--- lib/screens/Home/home_screen.dart
+++ lib/screens/Home/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../Details/categorydetails.dart';
 import '../Details/detailpage.dart';
+import '../authors_details/author_info.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,6 +13,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Map<String, dynamic>? bestAuthor; // Variable to hold the best author details
   List<Map<String, String>> categories = [];
   List<dynamic> readingHistory = [];
   List<dynamic> popularBooks = [];
@@ -25,12 +27,41 @@ class _HomeScreenState extends State<HomeScreen> {
     _fetchCategories();
     _fetchReadingHistory();
     fetchBooks();
+    fetchBestAuthor();
   }
+
 
   Future<String?> _getAuthToken() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString('auth_token');
   }
+
+  List<dynamic> topAuthors = []; // List to store the top 5 authors
+
+  Future<void> fetchBestAuthor() async {
+    const String authorsUrl = 'https://readme-backend-zdiq.onrender.com/api/v1/authors/with-book-count';
+    try {
+      final response = await http.get(Uri.parse(authorsUrl));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        List<dynamic> authors = data['authors'];
+
+        // Sort authors by number of books in descending order
+        authors.sort((a, b) => b['numberOfBooks'].compareTo(a['numberOfBooks']));
+        // Get the top 5 authors
+        final topAuthorsList = authors.take(5).toList();
+
+        setState(() {
+          topAuthors = topAuthorsList; // Store the top authors in state
+        });
+      } else {
+        throw Exception('Failed to load authors');
+      }
+    } catch (e) {
+      print('Error fetching top authors: $e');
+    }
+  }
+
 
   Future<void> fetchBooks() async {
     const String booksUrl = 'https://readme-backend-zdiq.onrender.com/api/v1/books';
@@ -145,10 +176,11 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: ListView(
-        physics: BouncingScrollPhysics(), 
+        physics: BouncingScrollPhysics(),
         children: [
           SizedBox(height: 45),
 
+          // Title
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Text(
@@ -163,6 +195,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           SizedBox(height: 13),
 
+          // Search Bar
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Container(
@@ -188,6 +221,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           SizedBox(height: 20),
 
+          // Categories Section
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: sectionHeader("Categories"),
@@ -216,6 +250,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           SizedBox(height: 20),
 
+          // Reading History Section
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: sectionHeader("Reading History"),
@@ -245,6 +280,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           SizedBox(height: 20),
 
+          // Popular Books Section
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: sectionHeader("Popular Books"),
@@ -265,6 +301,8 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
 
+          // Add Top Authors Section at the Bottom
+          buildBestAuthorSection(),
           SizedBox(height: 50),
         ],
       ),
@@ -272,6 +310,82 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
 
+
+  Widget buildBestAuthorSection() {
+    if (topAuthors.isEmpty) {
+      return SizedBox.shrink(); // Return an empty widget if no authors are available
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Top Authors',
+            style: TextStyle(
+              fontSize: 18, // Suitable size for section title
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            height: 130, // Adjust height for the horizontal list
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: topAuthors.length,
+              itemBuilder: (context, index) {
+                final author = topAuthors[index];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AuthorInfoScreen(
+                          authorId: author['_id'], // Pass author ID
+                        ),
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 12.0), // Adjust spacing between authors
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ClipOval(
+                          child: CachedNetworkImage(
+                            imageUrl: author['profilePicture'],
+                            width: 60, // Profile picture size
+                            height: 60,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(height: 8), // Space between image and name
+                        Container(
+                          width: 70, // Fixed width to prevent overflow
+                          child: Text(
+                            author['fullName'],
+                            style: TextStyle(
+                              fontSize: 10, // Reduced font size for names
+                              fontWeight: FontWeight.bold,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            maxLines: 2, // Allow names to wrap to two lines
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
 
   Widget sectionHeader(String title) {
@@ -354,6 +468,10 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget bookCard(dynamic book) {
+    double rating = book['rating'] != null
+        ? double.tryParse(book['rating'].toString()) ?? 0.0
+        : 0.0;
+
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -364,27 +482,29 @@ class _HomeScreenState extends State<HomeScreen> {
         );
       },
       child: Container(
-        width: 160, 
-        height: 380, 
+        width: 160,
+        height: 110, // Adjusted height for added rating section
         margin: EdgeInsets.only(right: 16),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
           color: Colors.white,
+          border: Border.all(color: Colors.grey.shade300, width: 1), // Edge border
           boxShadow: [
             BoxShadow(
               color: Colors.black.withOpacity(0.1),
-              blurRadius: 6,
-              offset: Offset(0, 4),
+              offset: Offset(0, 2), // Slight shadow for elevation
+              blurRadius: 4,
             ),
           ],
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Book Image
             ClipRRect(
               borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
               child: Container(
-                height: 90, 
+                height: 100, // Adjusted height for the image
                 width: double.infinity,
                 child: Image.network(
                   book['image'] ?? 'https://via.placeholder.com/150',
@@ -392,27 +512,39 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
-            Expanded( 
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      book['title'] ?? 'Unknown Title',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 3),
-                    Text(
-                      'By ${(book['authors'] as List).map((e) => e['fullName']).join(", ")}',
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 9, color: Colors.grey),
-                    ),
-                  ],
-                ),
+            // Book Details
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Book Title
+                  Text(
+                    book['title'] ?? 'Unknown Title',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 3),
+                  // Author(s)
+                  Text(
+                    'By ${(book['authors'] as List).map((e) => e['fullName']).join(", ")}',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 9, color: Colors.grey),
+                  ),
+                  SizedBox(height: 5),
+                  // Rating Section
+                  Row(
+                    children: List.generate(5, (index) {
+                      return Icon(
+                        Icons.star,
+                        size: 11, // Suitable size for the stars
+                        color: index < rating ? Colors.amber : Colors.grey.shade300,
+                      );
+                    }),
+                  ),
+                ],
               ),
             ),
           ],
@@ -420,8 +552,6 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-
-
 
   Widget readingHistoryCard(String title, String imageUrl, String bookId) {
     return GestureDetector(
