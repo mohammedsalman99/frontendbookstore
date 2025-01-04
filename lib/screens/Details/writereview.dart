b0d4f1bf4c83ed041806f78a4066e6b0005b0656
mohammed salman <159923000+mohammedsalman99@users.noreply.github.com>
2025-01-04 18:48:52 +0200

    Update writereview.dart

--- lib/screens/Details/writereview.dart
+++ lib/screens/Details/writereview.dart
@@ -80,12 +80,12 @@ class _WriteReviewPageState extends State<WriteReviewPage> {
 
         widget.refreshBookDetails();
         Future.delayed(Duration(seconds: 2), () {
-          Navigator.pop(context); // Close the review page
+          Navigator.pop(context);
         });
       } else if (response.statusCode == 400) {
         final responseBody = jsonDecode(response.body);
         final errorMessage = responseBody['message'] ?? "Your review contains inappropriate content.";
-        final isDuplicate = errorMessage.contains("duplicate"); // Check for duplicate-specific message
+        final isDuplicate = errorMessage.contains("duplicate"); 
 
         if (isDuplicate) {
           _showAdvancedDialog(
@@ -200,7 +200,7 @@ class _WriteReviewPageState extends State<WriteReviewPage> {
                   SizedBox(height: 20),
                   ElevatedButton(
                     onPressed: () {
-                      Navigator.pop(context); // Close dialog
+                      Navigator.pop(context); 
                     },
                     style: ElevatedButton.styleFrom(
                       padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
